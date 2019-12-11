9f60b514f0e0aef6546ed575465fe27dc353af7b
Cary Wolff <carywolff@northwesternmutual.com>
2019-12-11 12:05:13 -0600

    adding expression support

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const kenum = new Proxy({}, {
-	get: (obj, prop) => ([string]) => string
-		.split(/[\n\s]+/)
-		.filter(Boolean)
-		.reduce((obj, key) => (obj[key] = `${prop}/${key}`, obj), {})
+  get: (_, prop) => ([string, ...exp]) => {
+    let cleaned
+    let staged
+
+    exp.forEach(arr => cleaned = arr.split(/[\n\s]+/).filter(Boolean).reduce((obj, key) => (obj[key] = `${prop}/${key}`, obj), {}))
+
+    staged = string
+      .split(/[\n\s]+/)
+      .filter(Boolean)
+      .reduce((obj, key) => (obj[key] = `${prop}/${key}`, obj), {})
+
+
+    console.log("CLEANED: ", cleaned)
+    console.log("STAGED: ", staged)
+
+    const val = Object.assign({}, cleaned, staged)
+
+    console.log('VAL: ', val)
+
+    return val
+  }
 })
 
 module.exports = kenum
--- index.spec.js
+++ index.spec.js
@@ -31,4 +31,37 @@ describe('kenum', () => {
       YUM: 'FOO/YUM'
     })
   })
+  it('should work with a inserted variable', () => {
+    const VAR = 'VAR'
+    const result = kenum.FOO`
+      BAR
+      ${VAR}
+      PIE
+      YUM
+    `
+    expect(result).toEqual({
+      BAR: 'FOO/BAR',
+      VAR: 'FOO/VAR',
+      PIE: 'FOO/PIE',
+      YUM: 'FOO/YUM'
+    })
+  })
+  it('should work with inserted variables', () => {
+    const VAR = 'VAR'
+    const LET = 'LET'
+    const result = kenum.FOO`
+      BAR
+      ${VAR}
+      PIE
+      ${LET}
+      YUM
+    `
+    expect(result).toEqual({
+      BAR: 'FOO/BAR',
+      VAR: 'FOO/VAR',
+      PIE: 'FOO/PIE',
+      LET: 'FOO/LET',
+      YUM: 'FOO/YUM'
+    })
+  })
 })
