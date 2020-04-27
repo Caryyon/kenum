39f3c59f9db37c2dfae72e1a22180bc3e23ba736
Cary Wolff <carywolff@northwesternmutual.com>
2020-04-26 23:15:05 -0500

    feat: multiple located variables

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const _ = require('lodash')
+const flatten = require("lodash.flatten");
 
-const kenum = new Proxy({}, {
-  get: (x, prop) => (string, ...exp) => {
-    const merged = [...string, ...exp]
-    let cleaned = merged.map(str => str.split(/[\n\s]+/).filter(Boolean))
-    const lean = _.flatten(cleaned)
-    return lean.reduce((obj, key) => (obj[key] = `${prop}/${key}`, obj), {})
+const kenum = new Proxy(
+  {},
+  {
+    get: (x, prop) => (strings, ...exp) => {
 
 
+
+
+// Testing this shit
+      let result = [strings, exp]
+        .reduce((r, a) => (a.forEach((a, i) => (r[i] = r[i] || []).push(a)), r), [])
+        .reduce((a, b) => a.concat(b));
+
+
+      // Safe stuff below here
+      const merged = [...strings, ...exp];
+      const cleaned = merged.map((str) => str.split(/[\n\s]+/).filter(Boolean));
+      const lean = flatten(cleaned);
+      return lean.reduce(
+        (obj, key) => ((obj[key] = `${prop}/${key}`), obj),
+        {}
+      );
+    },
   }
-})
+);
 
-module.exports = kenum
+module.exports = kenum;
