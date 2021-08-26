df4be539b20275c9feecf042b0f97cf30f94238e
Cary Wolff <boss@caryyon.com>
2021-08-25 21:17:33 -0500

    few more tests for specific cases

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-const flatten = require('lodash.flatten')
+const flatten = require("lodash.flatten");
 
 const kenum = new Proxy(
   {},
   {
-    get: (x, prop) => (strings, ...exp) => {
-      // Safe stuff below here
-      const parsed = strings
-        .reduce((acc, val, idx) => acc.concat(val + (exp[idx] ? exp[idx] : '')), [])
-        .join('')
-        .split(/[\n\s]+/)
-        .filter(Boolean)
+    get:
+      (target, prop) =>
+      (strings, ...exp) => {
+        // Safe stuff below here
+        if (strings === undefined) {
+          console.log(
+            "it appears you are calling kenum as a function please use template literals"
+          );
+          return {};
+        }
+        const parsed = strings
+          .reduce(
+            (acc, val, idx) => acc.concat(val + (exp[idx] ? exp[idx] : "")),
+            []
+          )
+          .join("")
+          .split(/[\n\s]+/)
+          .filter(Boolean);
 
-      const lean = flatten(parsed)
-      return lean.reduce((obj, key) => ((obj[key] = `${prop}/${key}`), obj), {})
-    },
+        const lean = flatten(parsed);
+        return lean.reduce(
+          (obj, key) => ((obj[key] = `${prop}/${key}`), obj),
+          {}
+        );
+      },
   }
-)
+);
 
-module.exports = kenum
+module.exports = kenum;
